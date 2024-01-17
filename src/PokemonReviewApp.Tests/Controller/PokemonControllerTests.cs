b1ba36c791d1ad127837d63bda7cf777b6a088ac
BaoBiBo <ng.hoangbao03@gmail.com>
2024-01-17 19:38:06 +0700

    update

---
+++ src/PokemonReviewApp.Tests/Controller/PokemonControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using PokemonReviewApp.Controllers;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PokemonReviewApp.Tests.Controller
+{
+    public class PokemonControllerTests
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+        public PokemonControllerTests()
+        {
+            _pokemonRepository = A.Fake<IPokemonRepository>();
+            _reviewRepository = A.Fake<IReviewRepository>();
+            _mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public void PokemonController_GetPokemons_ReturnOk()
+        {
+            //Arrange
+            var pokemons = A.Fake<ICollection<PokemonDto>>();
+            var pokemonList = A.Fake<List<PokemonDto>>();
+            A.CallTo(() => _mapper.Map<List<PokemonDto>>(pokemons)).Returns(pokemonList);
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+            //Act
+            var result = controller.GetPokemons();
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
